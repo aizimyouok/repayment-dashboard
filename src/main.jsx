d3819b00a090b9a0bb6afb0f4830afef7d9a3aba
aizimyouok <aizimyouok46@gmail.com>
2025-07-08 14:55:30 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
+// =========================================================
+//  3. src/main.jsx - /src 폴더 안에 위치시킵니다.
+// =========================================================
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css' // 기본적인 스타일링을 위해 포함
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
